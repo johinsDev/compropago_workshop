b86aacce3d415cdd69ebf214d7c871aefe502799
JohinsDev <johinsdev@gmail.com>
2017-06-08 17:26:51 -0500

    add form purchase product

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170608154347) do
+ActiveRecord::Schema.define(version: 20170608190439) do
 
   create_table "products", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,18 @@ ActiveRecord::Schema.define(version: 20170608154347) do
     t.integer "princing"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "purchases", force: :cascade do |t|
+    t.string "email"
+    t.integer "product_id"
+    t.string "compropago_id"
+    t.boolean "status"
+    t.string "store"
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_purchases_on_product_id"
   end
 
 end
